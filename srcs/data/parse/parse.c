3fa89e4a166f9012d4ebe3386b2f1487c3ceccce
ppetitea <pierre.petiteau.4985@gmail.com>
2020-04-12 00:00:01 +0200

    just a save

--- srcs/data/parse/parse.c
+++ srcs/data/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: ppetitea <ppetitea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/11 17:54:30 by ppetitea          #+#    #+#             */
-/*   Updated: 2020/04/11 18:30:16 by ppetitea         ###   ########.fr       */
+/*   Updated: 2020/04/11 23:57:32 by ppetitea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,25 +42,114 @@ t_data	*parse_object()
 	
 }
 
-//					YOU ARE HERE !
+t_data	*parse_key(t_token *token, t_data *parent)
+{
+	t_data	*new;
+
+	if (!(new = init_new_data()))
+		return (console(FATAL, __func__, __LINE__, "newdata fail").null);
+	if (!(new->key = ft_strdup(token->data)))
+		return (console(FATAL, __func__, __LINE__, "strdup fail").null);
+	node_add_child(&parent->node, &new->node);
+	return (new);
+}
+
+/*
+** ANALYSE TOKENS
+*/
+
+t_bool		token_is_key(t_token *token)
+{
+	return (token->type == TOKEN_STRING &&
+		((t_token*)token->node.next)->type == TOKEN_COLON);
+}
+
+t_bool		token_is_integer(t_token *token)
+{
+	if (token->type != TOKEN_LESS && token->type != TOKEN_NUMBER)
+		return (FALSE);
+	token = (t_token*)token->node.next;
+	if (token->type != TOKEN_LESS && token->type != TOKEN_NUMBER)
+		return (FALSE);
+	return (token->type == TOKEN_STRING &&
+		((t_token*)token->node.next)->type == TOKEN_COLON);
+}
+
+t_bool		token_is_float(t_token *token)
+{
+	return (token->type == TOKEN_STRING &&
+		((t_token*)token->node.next)->type == TOKEN_COLON);
+}
+
+/*
+** PARSE TOKENS
+*/
 
 t_result	parse(t_token *token, t_data *parent)
 {
-	t_node	*curr;
 	t_data	*new;
 
-	if (token == NULL)
-		return (console(FATAL, __func__, __LINE__, "NULL pointer").err);
-	curr = token->node.childs;
-	while ((curr = curr->next) != token->node.childs)
+	if (token->type == TOKEN_STRING && ((t_token*)token->node.next)->type == TOKEN_COLON)
 	{
-		if (token->type == TOKEN_CURLY_BRACES_OPEN)
-		{
-			if (!(new = init_new_data()))
-				return (console(FATAL, __func__, __LINE__, "newdata fail").err);
-			node_add_child(&parent->node, &new->node);
-			parse((t_token*)curr, new);
-		}
+		new = parse_key(token, parent);
+		token = (t_token*)token->node.next->next;
 	}
+	if (token->type == TOKEN_NUMBER)
+	{
+		if (!(new = init_new_data()))
+			return (console(FATAL, __func__, __LINE__, "newdata fail").err);
+		if (!(new->key = ft_strdup(token->data)))
+			return (console(FATAL, __func__, __LINE__, "strdup fail").err);
+		node_add_child(&parent->node, &new->node);
+		token = (t_token*)token->node.next->next;
+	}
+	else if (token->type == TOKEN_CURLY_BRACES_OPEN)
+	{
+		new->type = LIST;
+		if (!parse((t_token*)token, new))
+			return (console(FATAL, __func__, __LINE__, "parse fail").err);
+		token = (t_token*)token->node.next;
+	}
+	else if (token->type == TOKEN_CURLY_BRACES_CLOSE)
+		return (OK);
 	return (OK);
 }
+
+// t_result	parse(t_token *token_parent, t_data *parent)
+// {
+// 	t_data	*new;
+// 	t_node	*curr;
+// 	t_token	*token;
+
+// 	if (token_parent == NULL)
+// 		return (console(FATAL, __func__, __LINE__, "NULL pointer").err);
+// 	curr = token_parent->node.childs;
+// 	while ((curr = curr->next) != token_parent->node.childs)
+// 	{
+// 		token = (t_token*)curr;
+// 		if (token->type == TOKEN_STRING && ((t_token*)token->node.next)->type == TOKEN_COLON)
+// 		{
+// 			new = parse_key(token, parent);
+// 			token = (t_token*)token->node.next->next;
+// 		}
+// 		if (token->type == TOKEN_NUMBER)
+// 		{
+// 			if (!(new = init_new_data()))
+// 				return (console(FATAL, __func__, __LINE__, "newdata fail").err);
+// 			if (!(new->key = ft_strdup(token->data)))
+// 				return (console(FATAL, __func__, __LINE__, "strdup fail").err);
+// 			node_add_child(&parent->node, &new->node);
+// 			token = (t_token*)token->node.next->next;
+// 		}
+// 		else if (token->type == TOKEN_CURLY_BRACES_OPEN)
+// 		{
+// 			new->type = LIST;
+// 			if (!parse((t_token*)token, new))
+// 				return (console(FATAL, __func__, __LINE__, "parse fail").err);
+// 			token = (t_token*)token->node.next;
+// 		}
+// 		else if (token->type == TOKEN_CURLY_BRACES_CLOSE)
+// 			return (OK);
+// 	}
+// 	return (OK);
+// }
